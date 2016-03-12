22966b8db2821fd3a954cd04ea0bcfc3d9520999
Prajwal <prajwalrenukanand@gmail.com>
2016-03-12 15:39:50 +0530

    Converted to a PyPI package

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from distutils.core import setup
 setup(
   name = 'trackit',
-  packages = ['trackit'], # this must be the same as the name above
+  packages = ['trackit'],
   version = '0.1',
   description = 'Tracking APIs for couriers with major courier companies',
   author = 'K R Prajwal',
   author_email = 'prajwalrenukanand@gmail.com',
-  url = 'https://github.com/prajwalkr/track-it', # use the URL to the github repo
-  download_url = 'https://github.com/prajwalkr/track-it/tarball/0.1', # I'll explain this in a second
-  keywords = ['couriers', 'tracking', 'api', 'courier-tracking'], # arbitrary keywords
+  url = 'https://github.com/prajwalkr/track-it', 
+  download_url = 'https://github.com/prajwalkr/track-it/tarball/0.1', 
+  keywords = ['couriers', 'tracking', 'api', 'courier-tracking'], 
   classifiers = [],
+  install_requires=[
+      "beautifulsoup4==4.4.1",
+      "selenium==2.48.0",
+      "requests==2.2.1",
+      "python-dateutil==2.4.2",
+  ]
 )
