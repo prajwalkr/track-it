66ae9b53efcf92f442c20d97a904fa4f081ef498
Prajwal <prajwalrenukanand@gmail.com>
2016-03-12 13:58:07 +0530

    Converted to a PyPI package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'track-it',
+  packages = ['track-it'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Tracking APIs for couriers with major courier companies',
+  author = 'K R Prajwal',
+  author_email = 'prajwalrenukanand@gmail.com',
+  url = 'https://github.com/prajwalkr/track-it', # use the URL to the github repo
+  download_url = 'https://github.com/prajwalkr/track-it/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
